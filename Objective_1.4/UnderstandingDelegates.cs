0cd7127639f264bb4a364b13bc0743407a63081d
unknown <caio.devbr@gmail.com>
2018-07-01 21:23:27 -0300

    First Commit

---
+++ Objective_1.4/UnderstandingDelegates.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Objective_1._4
+{
+    public class UnderstandingDelegates
+    {
+
+        public UnderstandingDelegates()
+        {
+
+        }
+        //Usamos a keyword delegate para avisar o compilador que você esta criando um type delegate.
+        //Um delegate pode fazer uma chamada de diversos métodos de MESMA assinatura
+        
+        #region Listing 1-75 Using Delegate
+
+        public delegate int Calculate(int x, int y);
+
+        private int Somar(int x, int y)
+        {
+            return x + y;
+        }
+        private int Multiplicar(int x, int y)
+        {
+            return x * y;
+        }
+
+        public void UseDelegate()
+        {
+
+            Calculate calc = Somar;
+            Console.WriteLine(calc(3, 4));
+
+            calc = Multiplicar;
+            Console.WriteLine(calc(2, 4));
+
+        }
+
+        #endregion
+
+        #region Listing 1-76 A Multicast Delegate
+
+        public delegate void Del();
+
+        private void MethodOne()
+        {
+            Console.WriteLine("MethodOne");
+        }
+
+        private void MethodTwo()
+        {
+            Console.WriteLine("MethodTwo");
+        }
+
+        public Delegate GetInvocationListPartial(int index)
+        {
+            Del del = MethodOne;
+            del += MethodTwo;
+            return del.GetInvocationList()[index];
+        }
+
+        public void Multicast()
+        {
+            Del d = MethodOne;
+            d += MethodTwo;
+            d();
+
+            //Diplays
+            //MethodOne
+            //MethodTwo
+        }
+
+
+
+        #endregion
+
+        #region Listing 1-77 Covariance with delegates
+
+        //Pelo motivo de StreamWriter e StringWriter Herdar de TextWriter,você pode usar o delegate CovarianceDel com
+        //ambos os métodos
+
+        public delegate TextWriter CovarianceDel();
+
+        private StreamWriter MethodStream()
+        {
+            return null;
+        }
+        private StringWriter MethodString()
+        {
+            return null;
+        }
+
+      public TextWriter tel()
+        {
+            CovarianceDel del;
+            del = MethodStream;
+            del += MethodString;
+            return del();
+        }
+
+        #endregion
+
+        #region Listing 1-78 Contravariance With Delegates
+
+        //Pelo motivo do Método DoSomething poder trabalhar com um TextWriter,ele certeamento pode também
+        //trabalhar com um StreamWriter.Porque a Contravariancia,você pode chamar um delegate e passar uma
+        //instancia de StreamWriter para o método DoSomething
+
+        private void DoSomething(TextWriter tw)
+        {
+            //Do Something
+
+        }
+
+        public delegate void ContravarianceDelegate(StreamWriter tw);
+
+        public void UsingContravariance()
+        {
+            ContravarianceDelegate del;
+            del = DoSomething;
+        }
+       
+        #endregion
+
+
+    }
+}
---
+++ Objective_1.4/UsingLambdaExpressions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objective_1._4
+{
+    public class UsingLambdaExpressions
+    {
+        public UsingLambdaExpressions()
+        {
+
+        }
+        //As vezes a Assinatura  de um método pode conter mais código do que o corpo do método.
+        //Há também situações em que você precisa criar um método inteiro apenas para usá-lo em um
+        //delegate.
+        public delegate int Calculate(int x, int y);
+
+        #region Listing 1-79 Lambda epression to create a delegate
+
+
+        public void UsingDelegatesWithLambda(int firstNum,int SecondNum)
+        {
+            //Ler a sintaxe lambda do seguinte jeito:
+            //x e y vai para a soma de x e y
+            Calculate calc = (x, y) => x + y;
+            Console.WriteLine("Resultado de uma Expressão Lambda de Soma: " + calc(firstNum, SecondNum));
+
+
+            //Ler a sintaxe lambda do seguinte jeito:
+            //x e y vai para o produto de x e y
+            calc = (x, y) => x * y;
+            Console.WriteLine("Resultado de uma Expressão Lambda de Multiplicação: " + calc(firstNum, SecondNum));
+        }
+
+        #endregion
+
+        #region 1-80 Creating a lambda expression with multiple statements
+        Calculate calc = (x, y) =>
+        {
+            Console.WriteLine("Adding numbers");
+            return x + y;
+        };
+        public int ResultOfMultipleStatements()
+        {
+            return calc(3, 4);
+        }
+
+
+        #endregion
+
+        #region Using The Action Delegate
+        //Se voce precisa de um delegate type que não retorna um vaor,você pode usar o System.Action types.Eles podem pegar 
+       //de 0 a 16 parametros,mas eles não podem retornar um valor
+
+        Action<int, int> calcAction = (x, y) =>
+        {
+            Console.WriteLine(x + y);
+        };
+        public void UsingActionDelegate()
+        {
+             calcAction(3, 4);
+        }
+
+        #endregion
+
+    }
+}
